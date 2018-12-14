739f00766353bc3d374d0675f5bea879a9a132f9
Andrew Peterson <andrew@andpeterson.com>
2018-12-13 16:15:51 -0800

    Updated Login Form Error, Created CreateAccountForm div

--- src/components/CreateAccountForm/CreateAccountForm.js
+++ src/components/CreateAccountForm/CreateAccountForm.js
@@ -4,38 +4,40 @@ import TextField from '@material-ui/core/TextField';
 export class CreateAccountForm extends React.Component{
     render(){
         return (
-            <TextField
-            id="SSID"
-            label=""
-            placeholder="SSID"
-            className="TextField"
-            margin="normal"
-            autoFocus
-            />
-            <TextField
-            id="FirstName"
-            label=""
-            placeholder="First Name"
-            className="TextField"
-            margin="normal"
-            autoFocus
-            />
-            <TextField
-            id="LastName"
-            label=""
-            placeholder="LastName"
-            className="TextField"
-            margin="normal"
-            autoFocus
-            />
-            <TextField
-            id="Email"
-            label=""
-            placeholder="E-mail"
-            className="TextField"
-            margin="normal"
-            autoFocus
-            />
+            <div class="CreateAccountForm">
+               <TextField
+               id="SSID"
+               label=""
+               placeholder="SSID"
+               className="TextField"
+               margin="normal"
+               autoFocus
+               />
+               <TextField
+               id="FirstName"
+               label=""
+               placeholder="First Name"
+               className="TextField"
+               margin="normal"
+               autoFocus
+               />
+               <TextField
+               id="LastName"
+               label=""
+               placeholder="LastName"
+               className="TextField"
+               margin="normal"
+               autoFocus
+               />
+               <TextField
+               id="Email"
+               label=""
+               placeholder="E-mail"
+               className="TextField"
+               margin="normal"
+               autoFocus
+               />
+            </div>
         )
     }
 }
