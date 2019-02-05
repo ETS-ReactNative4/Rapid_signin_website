482b18234c03c9ab8fa1ce2feb1aa49aefb4ccdb
Andrew Peterson <andrew@andpeterson.com>
2019-02-04 19:19:29 -0800

    Added handle Change and handleSubmit to Create Account Form

--- src/components/CreateAccountForm/CreateAccountForm.js
+++ src/components/CreateAccountForm/CreateAccountForm.js
@@ -2,12 +2,42 @@ import React from 'react'
 import TextField from '@material-ui/core/TextField';
 
 export class CreateAccountForm extends React.Component{
+    constructor(props){
+      super(props);
+      this.state = {ssid: "", first: "", last: "", email: ""};
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+      const target = event.target;
+
+      this.setSate({
+        ssid: target.ssid,
+        first: target.first,
+        last: target.last,
+        email: target.email
+      });
+    }
+
+    handleSubmit(event){
+      const state = this.state;
+      window.location = window.location +
+                        "/new_student?ssid=" + state.ssid +
+                        "&first" + state.first +
+                        "&last" + state.last +
+                        "&email" + state.email;
+      event.preventDefault();
+    }
+
+
     render(){
         return (
-            <div class="CreateAccountForm">
+            <form class="CreateAccountForm" onSubmit={this.handleSubmit}>
                <TextField
                id="SSID"
-               label=""
+               label={this.state.ssid}
                placeholder="SSID"
                className="TextField"
                margin="normal"
@@ -16,7 +46,7 @@ export class CreateAccountForm extends React.Component{
                <br />
                <TextField
                id="FirstName"
-               label=""
+               label={this.state.first}
                placeholder="First Name"
                className="TextField"
                margin="normal"
@@ -24,7 +54,7 @@ export class CreateAccountForm extends React.Component{
                />
                <TextField
                id="LastName"
-               label=""
+               label={this.state.last}
                placeholder="LastName"
                className="TextField"
                margin="normal"
@@ -33,13 +63,14 @@ export class CreateAccountForm extends React.Component{
                <br />
                <TextField
                id="Email"
-               label=""
+               label={this.state.email}
                placeholder="E-mail"
                className="TextField"
                margin="normal"
                autoFocus
                />
-            </div>
+              <input type="submit" value="Submit"/>
+            </form>
         )
     }
 }
