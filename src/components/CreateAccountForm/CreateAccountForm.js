9469f3f624a2cc89fd5b82c92b155b40661dcbd3
Andrew Peterson <andrew@andpeterson.com>
2019-02-04 20:51:01 -0800

    switching everything to draw from target.value

--- src/components/CreateAccountForm/CreateAccountForm.js
+++ src/components/CreateAccountForm/CreateAccountForm.js
@@ -14,10 +14,10 @@ export class CreateAccountForm extends React.Component{
       const target = event.target;
 
       this.setState({
-        ssid: target.ssid,
-        first: target.first,
-        last: target.last,
-        email: target.email
+        ssid: target.value,
+        first: target.value,
+        last: target.value,
+        email: target.value
       });
     }
 
